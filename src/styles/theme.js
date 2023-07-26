c9799e3ad05ccffcc7c85978bd501a03d1e3d784
mathias <mathias15052004@gmail.com>
2023-07-26 15:42:38 -0300

    Created styles theme and global files

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {
+        //DARK
+        DARK_400: "hsla(199, 100%, 3%, 1)",
+        DARK_900: "hsla(200, 48%, 10%, 1)",
+
+        //LIGHT
+        LIGHT_100: "hsla(0, 0%, 100%, 1)",
+        LIGHT_400: "hsla(240, 7%, 78%, 1)",
+        LIGHT_500: "hsla(240, 6%, 51%, 1)",
+
+        //TINT
+        TOMATO_100: "hsla(353, 95%, 24%, 1)",
+        CAKE_100: "hsla(195, 91%, 25%, 1)"
+    }
+}
